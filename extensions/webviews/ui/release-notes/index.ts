bc2d75c8a85603453a683f415721ce1002067691
LasaleFamine <info@godev.space>
2018-10-24 14:11:12 +0200

    chore: adding sanity client (waiting for bug fix from sanity)

--- extensions/webviews/ui/release-notes/index.ts
+++ extensions/webviews/ui/release-notes/index.ts
@@ -1,1 +1,17 @@
-console.log('ReleaseNotes');
+import * as sanityClient from '@sanity/client';
+
+const getClient = () => sanityClient({
+  projectId: 'v475t82f',
+  dataset: 'production',
+  token: 'skywTQVEqa902NDvMHoUATV9wSmVxPEdf93DVM3fvvXKSZnzGLMu0LzUNM96p8Uup2ZTBjIS5SbbQNZUSqU1KSKhnbWeMa2xhiaCprD8pcgOiWWN3r7sdppw6BgpMNT1zNJaWnyAKXmM3fjb0BNogddFHbKMOGv493lkVOTRBneZZ68tK8hj'
+});
+
+const getReleaseNotes = (): Promise<object[]> => {
+  const query = '*[_type == "release"] | order(publishedDateDesc)';
+  const client = getClient();
+  return client.fetch(query);
+};
+
+getReleaseNotes().then(res => {
+  res.forEach((item: any) => console.log(item.title));
+});
